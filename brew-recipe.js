ee7854502dc0744b2a1f3fae707886a2aab35d70
Adam Baranek <adam.baranek@ci-group.org>
2019-10-28 15:04:46 -0400

    Build basic HTML and CSS for the Home page

---
+++ brew-recipe.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+//Internal site navigation links
+let recommendLink = document.getElementById('recommend');
+recommendLink.addEventListener('click', () => window.location='http://www.google.com');
+
+let statsLink = document.getElementById('stats');
+statsLink.addEventListener('click', () => window.location='http://www.google.com');
