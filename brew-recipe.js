908c1081cb8bd30f0ef01308ddb4cabff4e70170
Adam Baranek <adam.baranek@ci-group.org>
2019-11-13 15:06:05 -0500

    Built Beer link button functionality

--- brew-recipe.js
+++ brew-recipe.js
@@ -24,11 +24,13 @@ function Brewbuilder(name, style, abv, rating) {
 	this.brewABV = abv;
 	this.brewRating = rating;
 }
-let beer001 = new Brewbuilder('Gluten Free Beer #1', 'Pale Ale', '4.8%', finalRatings.beer1);
-let beer002 = new Brewbuilder('Gluten Free Beer #2', 'IPA', '5.5%', finalRatings.beer2);
+let beerLink1 = new Brewbuilder('Gluten Free Beer #1', 'Pale Ale', '4.8%', finalRatings.beer1);
+let beerLink2 = new Brewbuilder('Gluten Free Beer #2', 'IPA', '5.5%', finalRatings.beer2);
 //Variables for beer and cider object storage
-let beerBrews = [beer001, beer002];
-let ciderBrews = ['Cider #1', 'Cider #2', 'Cider #3', 'Cider #4', 'Cider #5'];
+let beerBrews = [beerLink1, beerLink2];
+let ciderBrews = [];
+
+
 
 
 
@@ -40,10 +42,12 @@ let statsLink = document.getElementById('stats');
 statsLink.addEventListener('click', () => window.location='brew-stats.html');
 
 let beerLink = document.getElementById('beer');
-beerLink.addEventListener('click', beerRecipes());
+beerLink.addEventListener('click', beerRecipes);
 
-let ciderLink = document.getElementById('cider');
-ciderLink.addEventListener('click', );
+//let ciderLink = document.getElementById('cider');
+//ciderLink.addEventListener('click', );
+
+
 
 
 
@@ -51,14 +55,21 @@ ciderLink.addEventListener('click', );
 function beerRecipes() {
 	document.getElementById('main-content').innerHTML = '';
 
-	for (let i = 0; i <= beerBrews.length; i++) {
-		document.getElementById('main-content').innerHTML = `
-		<div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+	for (let i = 0; i < beerBrews.length; i++) {
+		document.getElementById('main-content').innerHTML += `
+		<div class="beer-links-header">
+			<div id="${'beer-links-' + (i + 1)}">${beerBrews[i].brewName}</div>
+			<div>${beerBrews[i].brewStyle}</div>
+			<div>${beerBrews[i].brewABV}</div>
+			<div>${beerBrews[i].brewRating}</div>
 		</div>
 		`;
 	}
+
+	//Beer Links to navigate to recipe page for each beer
+	let beerLinks1 = document.getElementById('beer-links-1');
+	beerLinks1.addEventListener('click', () => window.location='test-recipe.html');
+	let beerLinks2 = document.getElementById('beer-links-2');
+	beerLinks2.addEventListener('click', () => window.location='test-recipe.html');
 }
+
