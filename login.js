37e65117a40d4c642733e4e566fc6513ac56a0ef
Adam Baranek <adam.baranek@ci-group.org>
2019-11-22 12:35:41 -0500

    Finished basic setup for login page including JavaScript

--- login.js
+++ login.js
@@ -1,25 +1,21 @@
-function loginFun() {
+function loginTest() {
 	let userName = document.getElementById('username').value;
 	let userPassword = document.getElementById('password').value;
-//	let userRegex = /abaranek/;
-//	let passRegex = /bEErpASs2365ab/;
-	let userRegex = /a/;
-	let passRegex = /b/;
+	let userRegex = /abaranek/;
+	let passRegex = /bEErpASs2365ab/;
 
-	if (userName == 'a') {
-		//window.location = 'brew-recipes.html';
-		console.log(userName + ' ' + userPassword);
+	if (userRegex.test(userName) && passRegex.test(userPassword)) {
+		window.location = 'brew-recipes.html';
 	}
 	else {
 		document.getElementById('loginError').innerHTML = 'Username or Password is incorrect - Please try again.';
+		let resetButton = document.getElementById('username');
+		resetButton.addEventListener('click', () => document.getElementById('loginError').innerHTML = '');
+		let resetButton2 = document.getElementById('password');
+		resetButton2.addEventListener('click', () => document.getElementById('loginError').innerHTML = '');
 	}
-
-	
 }
 
 let loginButton = document.getElementById('login');
-loginButton.addEventListener('click', loginFun);
+loginButton.addEventListener('click', loginTest);
 
-
-//function is not working for some reason.
-//it will run, and then quickly revert anything it did. 
